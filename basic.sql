50ca3d5055769b05b2401ec0646cf3ad37a569ce
youngjae124 <jyj1579@naver.com>
2025-12-26 11:40:34 +0900

    DB 시작

---
+++ basic.sql
@@ -0,0 +1,27 @@
+
+
+--SQL Quert 주석
+-- 한줄주석
+
+/*여러줄 주석
+여러줄 주석*/
+
+DB(DataBase) 데이터가 저장되는 공간
+DBMS (DataBase Management System) 데이터베이스 관리 시스템
+
+우리 PC-> WindowS OS->Oracle DBMS  <- 접속 툴(sqldeveloper, sqlplus, etc...)로 사용자가 접속후 사용
+                                    (SQL 쿼리로 DB 데이터를 사용/조작)
+                                    
+*접속 계정
+id : scott
+pw : tiger
+
+SQL (쿼리문)
+Structured Query Language
+
+DB 사용/조작 -> 데이터 조작 명령(쿼리문을 작성해서)
+CRUD
+Create 생성
+Read 조회(읽기)
+Update 수정
+Delete 삭제
