4686d428c77121e9e2eb145d0f84ab3a29835957
youngjae124 <jyj1579@naver.com>
2025-12-29 10:15:52 +0900

    퀴즈2 조회,정렬

---
+++ Quiz/quiz02.sql
@@ -0,0 +1,10 @@
+select * from emp;
+
+select * from emp order by ename ;
+
+select * from emp order by deptno ,sal desc;
+
+select * from emp where sal >2000 order by sal desc;
+
+select * from student where grade not in(1) order by birthday desc; 
+
