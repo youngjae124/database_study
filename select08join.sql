a5192a82bd6b88f7323bbaadb992371051e325a5
youngjae124 <jyj1579@naver.com>
2026-01-02 11:28:01 +0900

    SELECT 조인 설명 2

--- select08join.sql
+++ select08join.sql
@@ -74,4 +74,101 @@ from professor p LEFT OUTER JOIN student s
 ON s.profno = p.profno;
 
 
+--INNER
+select *
+from professor p, student s
+where p.profno = s.profno;
 
+--OUTER
+select *
+from student s, professor p
+where s.profno = p.profno(+);
+
+--학생 20 교수 16, INNER JOIN 연결가능 15
+select *
+from professor p, student s   
+where p.profno = s.profno(+);
+
+
+-----------------------------------------
+select * from department;  --학과 정보
+select * from student;
+select * from professor;
+
+select s.name, s.deptno1, d.dname
+from student s, department d
+where s.deptno1 = d.deptno;
+
+select s.name, p.name, d.dname
+from student s, professor p, department d
+where s.profno = p.profno
+AND s.deptno1 = d.deptno;
+
+select s.name, p.name, d.dname
+from 
+    student s INNER JOIN professor p
+    ON s.profno = p.profno
+    INNER JOIN department d
+    ON s.deptno1 = d.deptno;
+
+select s.name, p.name, d.dname
+from student s, professor p, department d
+where s.profno = p.profno
+AND s.deptno1 = d.deptno
+AND s.deptno1 = 101;  
+
+
+1) 학생명, 학생학과번호, 학생의 학과명, 교수명, 교수학과번호
+select s.name, s.deptno1, d.dname, p.name, p.deptno 
+from student s, professor p, department d
+where s.profno = p.profno
+AND s.deptno1 = d.deptno;
+
+2) 학생명, 학생학과번호, 교수의 학과명, 교수명, 교수학과번호
+select s.name, s.deptno1, d.dname, p.name, p.deptno 
+from student s, professor p, department d
+where s.profno = p.profno
+AND p.deptno = d.deptno;
+
+3) 학생명, 학생학과번호, 학생의학과명, 교수명, 교수학과번호, 교수의 학과명
+select 
+    s.name stu_name,  
+    s.deptno1, 
+    ds.dname stu_dept_name, 
+    p.name 교수이름, 
+    p.deptno, 
+    dp.dname 교수소속학과이름
+from student s, professor p, department ds, department dp
+where s.profno = p.profno  --학생 <-> 지도교수
+and s.deptno1 = ds.deptno  --학생학과 <-> 학과정보
+and p.deptno = dp.deptno   --교수학과 <-> 학과정보
+;
+
+----------------------------------------
+INNER JOIN   OUTER JOIN
+등가조인=     비등가조인
+
+select * from customer;
+select * from gift;
+
+select *
+from customer c, gift g
+where c.point BETWEEN g.g_start and g.g_end;
+
+select * from hakjum;
+
+SELF JOIN
+
+select * from emp;
+
+--select *
+select e1.empno, e1.ename 사원이름, e1.mgr, e2.empno, e2.ename 상사이름
+from emp e1, emp e2
+where e1.mgr = e2.empno;
+
+select *
+from emp2;
+
+select a.empno, a.name, a.pempno, b.empno, b.name
+from emp2 a, emp2 b
+where a.pempno = b.empno;
